95d1a44d45554eba110a3a2dfb98f0c6ac087194
Bettogc <betto793@gmail.com>
2014-12-04 12:38:22 -0600

    Blogger

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Javascript Code.
 $(document).ready(function () {
-  // $.get("_posts/2014-11-09.json", function(data){
-  //
-  //   var posts = JSON.parse(data);
-  //   $(".posts").html(posts['posts'][0]['title']);
-  //
-  // });
+   $.get("_posts/2014-11-09.json", function(data){
+  
+     var posts = JSON.parse(data);
+     $(".posts").html(posts['posts'][0]['title']);
+  
+   });
 
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
     $(".posts").html(posts['posts'][0]['title']);
-    //
+    
   });
 });
